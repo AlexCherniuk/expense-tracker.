89886a439c45c18cb42564f80e1629a523c749bc
AlexCherniuk <60302722+AlexCherniuk@users.noreply.github.com>
2022-03-04 15:55:23 +0300

    added first state

--- src/Expenses/ExpenseItem.js
+++ src/Expenses/ExpenseItem.js
@@ -1,16 +1,21 @@
 import './ExpenseItem.css'
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
+    const [title, setTitle] = useState(props.title)
 
+    const  clickHandler = () => {
+        setTitle('Good!');
+    }
     return (
         <Card className='expense-item'>
-            <ExpenseDate date={props.date}/>
+            <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
+                <button onClick={clickHandler}>Click me</button>
             </div>
         </Card>
     );
