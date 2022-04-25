e92c04be216b63e32daba92be7336b9e983ec0f4
AlexCherniuk <60302722+AlexCherniuk@users.noreply.github.com>
2022-04-25 17:20:46 +0200

    working filter without conditional

--- src/Expenses/ExpenseItem.js
+++ src/Expenses/ExpenseItem.js
@@ -1,21 +1,16 @@
 import './ExpenseItem.css'
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
-    const [title, setTitle] = useState(props.title)
 
-    const  clickHandler = () => {
-        setTitle('Good!');
-    }
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
-                <button onClick={clickHandler}>Click me</button>
             </div>
         </Card>
     );
